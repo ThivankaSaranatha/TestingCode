f787b009742a6401c998980e2810608d0550c6d0
ThivankaSaranatha <160152253+ThivankaSaranatha@users.noreply.github.com>
2024-12-03 21:03:13 +0100

    first commit

---
+++ LibrarySystem/Controllers/BooksController.cs
@@ -0,0 +1,30 @@
+﻿using LibrarySystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly List<Book> _books = [new()
+    {
+        BookId = 1,
+        BookName = "Life"
+    },
+    new ()
+    {
+        BookId = 2,
+        BookName = "Lessons"
+    },
+    new ()
+    {   BookId = 3,
+        BookName = "Learned"
+    }];
+
+    public IEnumerable<Book> GetAll()
+    {
+        return _books;
+    }
+}
---
+++ LibrarySystem/Models/Book.cs
@@ -0,0 +1,7 @@
+﻿namespace LibrarySystem.Models;
+
+public record Book
+{
+    public int BookId { get; init; }
+    public required string BookName { get; init; }
+}
